94529887f2d94da8c2d559a3b974af264ab12f6d
AbdelhalimA0 <abdelhalim.abdelwahab2@gmail.com>
2025-07-18 00:09:44 +0300

    first commit

---
+++ my.js
@@ -0,0 +1,5 @@
+let Voltage=window.prompt('Voltage');
+let Resistance=window.prompt('Resistance');
+console.log('V = '+Voltage);
+console.log('R = '+Resistance);
+console.log('I = ' +Voltage/Resistance);
